d2f48c2fc1ad8f4f8d7c9d6205b91dcff1f09a0b
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-08-26 21:34:39 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,124 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
-		}
-
-		auto sapphireBtn = CCMenuItemSpriteExtra::create(
-			CircleButtonSprite::createWithSpriteFrameName("sapphire.png"_spr, 1.2f, CircleBaseColor::Green, CircleBaseSize::MediumAlt),
-			this,
-			menu_selector(MyMenuLayer::onSapphire)
-		);
-
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(sapphireBtn);
-
-		sapphireBtn->setID("sapphire"_spr);
-
-		menu->updateLayout();
-
-		return true;
+class $modify(CreatorLayer) {
+	void onAdventureMap(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onAdventureMap"  // content
+		    "OK"        // button
+		)->show();
 	}
 
-	void onSapphire(CCObject*) {
-		geode::openModsList();
+	void onDailyLevel(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onDailyLevel"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onEventLevel(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onEventLevel"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onFeaturedLevels(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onFeaturedLevels"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onGauntlets(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onGauntlets"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onLeaderboards(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onLeaderboards"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onMapPacks(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onMapPacks"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onMultiplayer(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onMultiplayer"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onMyLevels(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onMyLevels"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onOnlineLevels(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onOnlineLevels"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onPaths(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onPaths"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onSavedLevels(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onSavedLevels"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onSecretVault(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onSecretVault"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onTopLists(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onTopLists"  // content
+		    "OK"        // button
+		)->show();
+	}
+
+	void onWeeklyLevel(CCObject*) {
+		FLAlertLayer::create(
+		    "Function Ran",    // title
+		    "onWeeklyLevel"  // content
+		    "OK"        // button
+		)->show();
 	}
 };
